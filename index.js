895b95c716c8c736f812a977aa659fe9e0ba3e3f
Shruti Dixit <shrutidixit5@gmail.com>
2022-05-30 22:47:47 -0700

    Styling changes and added Favicon

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-/* user pop up interaction when they access the website */
 
 $("#welcomeModal").modal("show");
+
+$("#feedbackForm").submit(function (event) {
+  alert("Thank you for your feedback! We will get back to you as soon as possible!");
+  event.preventDefault();
+  $('#feedbackForm')[0].reset();
+});
